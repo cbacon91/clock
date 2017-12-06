6d4a651dcc5b12a9311386e22bf0d81b53e53119
Chris Bacon <cbacon91@gmail.com>
2017-12-05 18:05:35 -0700

    add time, add isSameQueue check for cycledays looping

--- ballclock.go
+++ ballclock.go
@@ -1,16 +1,53 @@
 package clock
 
-import "errors"
+import (
+	"errors"
+	"time"
+)
 
 func main() {
 
 }
 
-// CycleDays ...
+type ball int
+
+// CycleDays takes a number of balls and determines the number of 24-hour periods which elapse before the clock returns to its initial ordering.
+// It returns the number of days taken to return to inital order, the time in seconds it took to run, and potential errors.
 func CycleDays(ballCount int) (int, float64, error) {
+	start := time.Now()
+
 	if ballCount < 27 || ballCount > 127 {
 		return 0, 0, errors.New("can only run with ballCount between 27 and 127")
 	}
 
-	return 0, 0, nil
+	q := make([]ball, ballCount)
+
+	for i := range q {
+		q[i] = ball(i)
+	}
+
+	// setup the initial queue structure
+	initialQ := q
+
+	for !IsSameQueue(initialQ, q) {
+		//run logic while it's not the same queue
+	}
+
+	elapsed := time.Since(start).Seconds()
+	return 0, elapsed, nil
+}
+
+// IsSameQueue comapres two slices of ball to verify same values and same ordering
+func IsSameQueue(q1, q2 []ball) bool {
+	if len(q1) != len(q2) {
+		return false
+	}
+
+	for i, v := range q1 {
+		if v != q2[i] {
+			return false
+		}
+	}
+
+	return true
 }
--- ballclock_test.go
+++ ballclock_test.go
@@ -18,15 +18,32 @@ func TestBallCount(t *testing.T) {
 		{128, 0, 0, "can only run with ballCount between 27 and 127"},
 	}
 	for _, c := range cases {
-		days, time, err := CycleDays(c.in)
+		days, _, err := CycleDays(c.in)
 		if days != c.days {
 			t.Errorf("CycleDays(%v) == %v, want %v (days)", c.in, days, c.days)
 		}
-		if time != c.time {
-			t.Errorf("CycleDays(%v) == %v, want %v (time)", c.in, time, c.time)
-		}
+		// no test case for time because it's variable by definition
 		if err != nil && c.err != "" && err.Error() != c.err {
 			t.Errorf("CycleDays(%v) == %v, want %v (err)", c.in, err.Error(), c.err)
+		}
+	}
+}
+
+func TestIsSameQueue(t *testing.T) {
+	cases := []struct {
+		q1, q2 []ball
+		result bool
+	}{
+		{[]ball{1, 2, 3}, []ball{1, 2, 3}, true},  // success
+		{[]ball{1, 2, 3}, []ball{3, 2, 1}, false}, // wrong order
+		{[]ball{1, 2, 3}, []ball{2, 2, 3}, false}, // not same elements
+		{[]ball{1, 2, 3}, []ball{2, 3}, false},    // q1 longer
+		{[]ball{2, 3}, []ball{1, 2, 3}, false},    // q2 longer
+	}
+	for _, c := range cases {
+		result := IsSameQueue(c.q1, c.q2)
+		if result != c.result {
+			t.Errorf("IsSameQueue(%v, %v) == %v, want %v ", c.q1, c.q2, result, c.result)
 		}
 	}
 }
