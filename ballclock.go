4baf4394bb775f76bdcdc019595581fc21c5accd
Chris Bacon <cbacon91@gmail.com>
2017-12-09 13:17:13 -0700

    finish logic of assignment
    finished basics of the assignment; matches expected cases. rewrote unit tests to be based around clock instead of json; need to restructure addMinute to work around track logic instead to be cleaner; need to unit test addMinute specifically

--- ballclock.go
+++ ballclock.go
@@ -1,6 +1,7 @@
 package clock
 
 import (
+	"encoding/json"
 	"errors"
 	"reflect"
 	"time"
@@ -10,30 +11,56 @@ func main() {
 
 }
 
-type ballClock struct {
-	main, minute, fiveMin, hour []int
+// BallClock is a thing
+type BallClock struct {
+	twelveHours              int
+	Main, Min, FiveMin, Hour []int
 }
 
-func (c *ballClock) addMinute() {
-	/*
-		if minute arm currently has 4
-			move balls in reverse order of addition to main //fn
-			//minuteballs := reverseSlice(c.minute)
-			c.minute.empty() / c.minute = []int
-			c.main = append(c.main, minuteBalls)
+func (c *BallClock) String() string {
+	jason, err := json.Marshal(c)
+	if err != nil {
+		panic(err)
+	}
 
-			if 5min arm has currently has 11
-				move balls in reverse order of addition to main
-				if hour arm has currently has 11
-					move balls in reverse order of addition to main
-					add ball to main
-				else
-					append ball to hour arm
-			else
-				append ball to 5min arm
-		else
-			append ball to minute arm
-	*/
+	return string(jason)
+}
+
+// AddMinute adds a minute to the ball clock. Attempts to move a ball from main to Min.
+// If Min is full, it appends Min to main in reverse order and attempts to move the
+// ball to fivemin instead. If fivemin is full, it repeats this process with hour. Finally,
+// if hour is full the clock is reset
+func (c *BallClock) AddMinute() {
+	//remove from main
+	newBall := c.Main[0] // take the least recently used ball
+	c.Main = c.Main[1:]
+
+	if len(c.Min) == 4 {
+
+		minuteBalls := ReverseSlice(c.Min)
+		c.Min = make([]int, 0)
+		c.Main = append(c.Main, minuteBalls...)
+
+		if len(c.FiveMin) == 11 {
+			fiveMinBalls := ReverseSlice(c.FiveMin)
+			c.FiveMin = make([]int, 0)
+			c.Main = append(c.Main, fiveMinBalls...)
+
+			if len(c.Hour) == 11 {
+				hourBalls := ReverseSlice(c.Hour)
+				c.Hour = make([]int, 0)
+				c.Main = append(c.Main, hourBalls...)
+				c.Main = append(c.Main, newBall)
+				c.twelveHours++
+			} else {
+				c.Hour = append(c.Hour, newBall)
+			}
+		} else {
+			c.FiveMin = append(c.FiveMin, newBall)
+		}
+	} else {
+		c.Min = append(c.Min, newBall)
+	}
 }
 
 // ReverseSlice takes a slice and returns a reversed copy of it.
@@ -49,7 +76,7 @@ func ReverseSlice(forward []int) []int {
 
 // CycleDays takes a number of balls and determines the number of 24-hour periods which elapse before the clock returns to its initial ordering.
 // It returns the number of days taken to return to inital order, the time in seconds it took to run, and potential errors.
-func CycleDays(ballCount int) (int, float64, error) {
+func CycleDays(ballCount int) (float32, float64, error) {
 	start := time.Now()
 
 	if ballCount < 27 || ballCount > 127 {
@@ -60,22 +87,45 @@ func CycleDays(ballCount int) (int, float64, error) {
 
 	// setup the initial stack structure to compare against
 	initialQ := make([]int, ballCount)
-	copy(initialQ, clock.main)
+	copy(initialQ, clock.Main)
 
 	//run first case before this, otherwise we have an infinite loop here
-	for ok := true; ok; ok = !reflect.DeepEqual(initialQ, clock.main) {
-		clock.addMinute()
+	for ok := true; ok; ok = !reflect.DeepEqual(initialQ, clock.Main) {
+		clock.AddMinute()
 	}
 
 	elapsed := time.Since(start).Seconds()
-	return 0, elapsed, nil
+	return float32(clock.twelveHours) / 2, elapsed, nil
 }
 
-func newClock(ballCount int) ballClock {
-	clock := ballClock{}
+// Clock is the clock mode.
+func Clock(ballCount, runToMinutes int) (BallClock, float64, error) {
+	start := time.Now()
+	var clock BallClock
+
+	if ballCount < 27 || ballCount > 127 {
+		return clock, 0, errors.New("can only run with ballCount between 27 and 127")
+	}
+	if runToMinutes < 1 {
+		return clock, 0, errors.New("must specify positive number of minutes to run for")
+	}
+
+	clock = newClock(ballCount)
+
+	//run first case before this, otherwise we have an infinite loop here
+	for i := 1; i <= runToMinutes; i++ {
+		clock.AddMinute()
+	}
+
+	elapsed := time.Since(start).Seconds()
+	return clock, elapsed, nil
+}
+
+func newClock(ballCount int) BallClock {
+	clock := BallClock{}
 
 	for i := 1; i <= ballCount; i++ {
-		clock.main = append(clock.main, i)
+		clock.Main = append(clock.Main, i)
 	}
 
 	return clock
--- ballclock_test.go
+++ ballclock_test.go
@@ -1,31 +1,53 @@
 package clock
 
 import (
+	"errors"
 	"reflect"
 	"testing"
 )
 
-func TestBallCount(t *testing.T) {
+func TestCycleDays(t *testing.T) {
 	cases := []struct {
-		in, days int
-		time     float64
-		err      string
+		in   int
+		days float32
+		err  error
 	}{
-		{26, 0, 0, "can only run with ballCount between 27 and 127"},
-		{27, 0, 0, ""},
-		{28, 0, 0, ""},
-		{126, 0, 0, ""},
-		{127, 0, 0, ""},
-		{128, 0, 0, "can only run with ballCount between 27 and 127"},
+		{26, 0, errors.New("can only run with ballCount between 27 and 127")},
+		{27, 23, nil},
+		{30, 15, nil},
+		{45, 378, nil},
+		{128, 0, errors.New("can only run with ballCount between 27 and 127")},
 	}
 	for _, c := range cases {
 		days, _, err := CycleDays(c.in)
 		if days != c.days {
 			t.Errorf("CycleDays(%v) == %v, want %v (days)", c.in, days, c.days)
 		}
-		// no test case for time because it's variable by definition
-		if err != nil && c.err != "" && err.Error() != c.err {
+		if !reflect.DeepEqual(err, c.err) { //!= nil && c.err != nil && err.Error() != c.err.Error() {
 			t.Errorf("CycleDays(%v) == %v, want %v (err)", c.in, err.Error(), c.err)
+		}
+	}
+}
+
+func TestClock(t *testing.T) {
+	cases := []struct {
+		balls, minutes int
+		expected       BallClock
+		err            error
+	}{
+		{26, 2, BallClock{}, errors.New("can only run with ballCount between 27 and 127")},
+		{128, 2, BallClock{}, errors.New("can only run with ballCount between 27 and 127")},
+		{30, 0, BallClock{}, errors.New("must specify positive number of minutes to run for")},
+		{30, -100, BallClock{}, errors.New("must specify positive number of minutes to run for")},
+		{30, 325, BallClock{0, []int{11, 5, 26, 18, 2, 30, 19, 8, 24, 10, 29, 20, 16, 21, 28, 1, 23, 14, 27, 9}, []int{}, []int{22, 13, 25, 3, 7}, []int{6, 12, 17, 4, 15}}, nil},
+	}
+	for _, c := range cases {
+		clock, _, err := Clock(c.balls, c.minutes)
+		if !reflect.DeepEqual(clock, c.expected) {
+			t.Errorf("Clock(%v, %v) == %v, want %v", c.balls, c.minutes, clock, c.expected)
+		}
+		if !reflect.DeepEqual(err, c.err) { //!= nil && c.err != nil && err.Error() != c.err.Error() {
+			t.Errorf("Clock(%v, %v) == %v, want %v", c.balls, c.minutes, err, c.err)
 		}
 	}
 }
